94eba2c2e8ee30b499af65156d171d71bc5f56af
Juank628 <jsanchez_zunino@yahoo.com>
2023-01-10 15:54:49 -0500

    CRA boilerplate files deleted and App component changed to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
